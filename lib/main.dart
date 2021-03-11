eae9afb1d77a3f09d801241bc4f6011c6c1cd91e
afreenfathimaa <afreenfathima968@gmail.com>
2021-03-11 20:20:45 +0530

    quizzler

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
 
-void main() => runApp(XylophoneApp());
+QuizBrain quizBrain = QuizBrain();
+void main() => runApp(Quizzler());
 
-class XylophoneApp extends StatelessWidget {
-
-  void playSound(int soundNumber) {
-     final player = AudioCache();
-     player.play('note$soundNumber.wav'); 
-  }
-  Expanded buildkey({Color color, int soundNumber}) {
-    return Expanded(
-       child:FlatButton(
-      color: color,
-      onPressed: () {
-        playSound(soundNumber);
-      },
-    ));
-  }
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget> [
-              buildkey(color: Colors.red, soundNumber: 1),
-              buildkey(color: Colors.green, soundNumber: 2),
-              buildkey(color: Colors.purple, soundNumber: 3),
-              buildkey(color: Colors.orange, soundNumber: 4),
-              buildkey(color: Colors.yellow, soundNumber: 5),
-              buildkey(color: Colors.pink, soundNumber: 6),
-              buildkey(color: Colors.blue, soundNumber: 7),
-            ],
-          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
         ),
-      );
+      ),
+    );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  
+
+  void checkAnswer(bool userPickedAnswer) {
+     bool correctAnswer = quizBrain.getCorrectAnswer();
+             setState(() {
+                   if (quizBrain.isFinished() == true) {
+                     Alert(
+                       context: context,
+                       title: 'Finished!',
+                       desc: 'You\'ve reached the end of the quiz.',
+                     ).show();
+                     
+                     quizBrain.reset();
+
+                      scoreKeeper = [];
+                   }
+                   else{
+                      if (userPickedAnswer == correctAnswer){
+                  scoreKeeper.add(Icon(Icons.check,color: Colors.green,));
+                }else{
+                  scoreKeeper.add(Icon(Icons.check,color: Colors.red,));
+                }
+               
+                 quizBrain.nextQuestion();
+               }
+             });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+               quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                
+              checkAnswer(true);
+               
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+           Row(
+             children:scoreKeeper,
+           )
+      ],
+    );
+  }
+}
+
